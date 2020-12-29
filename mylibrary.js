9187a684e1033e3e7606800fa089fbb362a9765e
ananyagraphic <75197504+ananyagraphic@users.noreply.github.com>
2020-12-29 18:55:02 +0530

    Add files via upload

---
+++ mylibrary.js
@@ -0,0 +1,16 @@
+function isTouching(){
+    if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  }
+  function createBox(){
+      box=createSprite(200,100,50,50)
+  }
